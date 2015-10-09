c3088a2245820bb2c671846a222a91a0521405da
Harvey Ngo <harveydngo@gmail.com>
2015-10-08 22:32:23 -0700

    Include new route to handle server side pagination.
    Implemented a new route that would take a page offset param and a search object id param. Given the params, the API will query the database for the corresponding search object, and retrieve a subset from that object's results. The subset begins with the offset and includes 19 additional results. Because of the undefined method error bug, the API tends to pause when it is unable successfully make a GET request to the Instagram API, causing the client side to think that there are no more images available for rendering. Will fix given time.

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -15,6 +15,21 @@ class ResultsController < ApplicationController
 		render json: @search.results
 	end
 
+	def show_page
+		@search = Search.find(params[:id])
+		result_offset = params[:result_offset].to_i
+		results_subset = @search.results[result_offset..(result_offset + 19)]
+		next_page_boolean = false;
+		if @search.results.length > result_offset + 10
+			next_page_boolean = true;
+		end
+		
+		# results.map!{ |result| result.to_json}
+		# next_media_set = results[page_num]
+		# render json: {search_id: @search.id, page: page_num, next_results: next_media_set}
+		render json: {search_id: @search.id, results: results_subset, next_page: next_page_boolean}
+	end
+
 	private
 		def result_params
 			params.require(:result).permit(:ig_username, :tag_time, :type, :ig_link, :image_url, :video_url, :description)
