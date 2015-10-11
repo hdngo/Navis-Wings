3a3a3951267b186f04fc90809de97eea04c8e1fa
Harvey Ngo <harveydngo@gmail.com>
2015-10-10 17:29:01 -0700

    Attempt to add client config blocks for Redis.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,10 @@
 uri = ENV["REDISTOGO_URL"] || "redis://localhost:6379/"
 REDIS = Redis.new(:url => uri)
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: "redis://localhost:6379/" }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: "redis://localhost:6379/" }
+end
